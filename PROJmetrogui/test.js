8faa603ffb08a15b738e02eab6193d81ff5edb9f
David Sklar <dfsklar@gmail.com>
2011-10-29 23:04:58 -0400

    finally now using the real tradegoth fonts

--- PROJmetrogui/test.js
+++ PROJmetrogui/test.js
@@ -7,6 +7,9 @@ $(document).ready
 		  // Pass the relative URL of the image containing the triangular "slipcover".
 		  INITCLASSmetrotilebase("triangle.png");
 
+
+
+
 		  new CLASSmetrocarousel 
 		  ("MYCITY",
 			[
@@ -47,6 +50,50 @@ $(document).ready
 			}
 		  );
 
+
+
+
+
+
+		  new CLASSmetrocarousel 
+		  ("education",
+			[
+				 {
+					  image: "UNIV_SMU.jpg",
+					  caption: "SMU, Dallas"
+				 },
+				 {
+					  image: "UNIV_Brown.jpg",
+					  caption: "Brown U, Providence"
+				 }
+			],
+			{
+				 "behavior-at-end": "loop",
+
+				 "duration-fadein-image":  3,
+				 "duration-fadein-banner": 3,
+				 "duration-fadein-text":   3,
+
+				 "algorithm-fadein-image":  PennerTween.linear,
+				 "algorithm-fadein-banner": PennerTween.linear,
+				 "algorithm-fadein-text":   PennerTween.linear,
+
+				 "duration-fadeout-image":  1,
+				 "duration-fadeout-banner": 1,
+				 "duration-fadeout-text":   1,
+
+				 "opacity-image":  100, // in percent
+				 "opacity-banner": 45,
+				 "opacity-text": 100,
+
+				 "duration-hold-on": 3,
+				 "duration-hold-off": 0,
+
+				 "duration-slipcover-movement": 2,
+
+				 "margin-below-banner": 18 //in pixels
+			}
+		  );
 
 
 
