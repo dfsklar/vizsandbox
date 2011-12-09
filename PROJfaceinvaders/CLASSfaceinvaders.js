a71cdf9bd0cf205c38cf8af3fedb9368d0856487
David Sklar <dfsklar@gmail.com>
2011-12-09 18:11:54 -0500

    new

---
+++ PROJfaceinvaders/CLASSfaceinvaders.js
@@ -0,0 +1,29 @@
+var CLASSfaceinvaders = Class.extend(
+	 {
+		  
+		  initialize: function(IDofCanvasDomnode)
+		  {
+				if (IDofCanvasDomnode) {
+					 this.IDofCanvasDomnode = IDofCanvasDomnode;
+				}
+
+				// CONSTRUCT THE STAGE
+				this.jqcanv = $JQ('#'+this.IDofCanvasDomnode);  //'CanvasBarChartGame'
+				this.canvas = this.jqcanv.get(0);
+				this.stage = new Stage(this.canvas);
+		  },
+
+
+		  adjustToWidth: function() 
+		  {
+				// The width of the entire "client area" of the browser
+				this.WIDTHwindow = this.jqcanv.get(0).width = window.innerWidth;
+				var width = this.WIDTHwindow;
+		  }
+
+
+		  
+	 }
+);
+
+
---
+++ PROJfaceinvaders/faceinvaders.js
@@ -0,0 +1,14 @@
+$(document).ready
+(
+	 function()
+	 {
+		  // PROGRAM INITIALIZATION GOETH HERE.
+		  // PROGRAM INITIALIZATION GOETH HERE.
+		  // PROGRAM INITIALIZATION GOETH HERE.
+
+		  $JQ = $;
+
+
+		  GAME = new CLASSfaceinvaders ("thecanvas");
+	 }
+);
