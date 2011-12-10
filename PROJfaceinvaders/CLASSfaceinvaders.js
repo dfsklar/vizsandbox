4422ad55089bf7c65cc17da7234035c85313e1e7
David Sklar <dfsklar@gmail.com>
2011-12-09 19:01:40 -0500

    cont

--- PROJfaceinvaders/CLASSfaceinvaders.js
+++ PROJfaceinvaders/CLASSfaceinvaders.js
@@ -1,5 +1,10 @@
 var CLASSfaceinvaders = Class.extend(
 	 {
+
+		  CFG: 
+		  {
+				"marginBottom": 5
+		  },
 		  
 		  initialize: function(IDofCanvasDomnode)
 		  {
@@ -10,7 +15,25 @@ var CLASSfaceinvaders = Class.extend(
 				// CONSTRUCT THE STAGE
 				this.jqcanv = $JQ('#'+this.IDofCanvasDomnode);  //'CanvasBarChartGame'
 				this.canvas = this.jqcanv.get(0);
+				this.gleanCanvasDimensions();
 				this.stage = new Stage(this.canvas);
+
+				
+				// CONSTRUCT THE SHOOTER in its own modeling coordsys
+				// 0,0 is the tip of the shooter, and its base is in positive-y territory
+				this.gameobjShooter = new Shape();
+				var GOBJ = this.gameobjShooter;
+				var x = 5;
+				GOBJ.graphics.beginFill("#3F352A")
+					 .moveTo(0,0)
+					 .lineTo(-x,x*1.2)
+					 .lineTo(x,x*1.2)
+					 .lineTo(0,0)
+					 .endFill();
+				GOBJ.x = (this.canvasWidth/2);
+				GOBJ.y = (this.canvasHeight - this.CFG.marginBottom);
+				this.stage.addChild(GOBJ);
+				this.stage.update();
 		  },
 
 
@@ -19,7 +42,16 @@ var CLASSfaceinvaders = Class.extend(
 				// The width of the entire "client area" of the browser
 				this.WIDTHwindow = this.jqcanv.get(0).width = window.innerWidth;
 				var width = this.WIDTHwindow;
+		  },
+
+
+		  gleanCanvasDimensions: function()
+		  {
+				this.canvasWidth = this.jqcanv.get(0).width;
+				this.canvasHeight = this.jqcanv.get(0).height;
 		  }
+
+		  
 
 
 		  
