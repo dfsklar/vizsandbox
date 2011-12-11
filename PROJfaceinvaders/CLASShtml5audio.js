87fbd669da5bd6c7e910fa10becdbb2d7b00b2b6
David Sklar <dfsklar@gmail.com>
2011-12-11 10:50:25 -0500

    commit

--- PROJfaceinvaders/CLASShtml5audio.js
+++ PROJfaceinvaders/CLASShtml5audio.js
@@ -5,25 +5,8 @@ var SINGLETONhtml5audio = null;
 var CLASShtml5audio = Class.extend(
 	 {
 
-		  // CODE WILL BE STOLEN FROM:
+		  // CODE BORROWED FROM:
 		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-		  //   view-source:http://soundjs.com/examples/TestSuite.html
-
 
 
 		  // Each value in the array must be a map having these values:
