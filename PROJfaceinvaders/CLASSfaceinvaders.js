eb6c9f744f6f5a5baefd0b82809ac1e6d490694e
David Sklar <dfsklar@gmail.com>
2011-12-14 01:57:28 -0500

    commit

--- PROJfaceinvaders/CLASSfaceinvaders.js
+++ PROJfaceinvaders/CLASSfaceinvaders.js
@@ -386,7 +386,7 @@ var CLASSfaceinvaders = Class.extend(
 						  ,  
 						  {
 								bitmap: 
-								(rownum > 3) ? "row1and2.png" : "row4and3.png"
+								(rownum > 3) ? "example1.png" : "row4and3.png"
 						  }
 					 );
 				return x;
---
+++ PROJfaceinvaders/givefacetoalien.pl
@@ -0,0 +1,18 @@
+use Image::Magick;
+
+$filename = "example2";
+
+$x = Image::Magick->new;
+$x->Read("alien12_50x50.bmp");
+$x->Read($filename.".jpg");
+
+$x->[0]->Composite(
+	 image => $x->[1],
+	 compose => "Minus"
+	 );
+
+$x->[0]->Scale(geometry => "100%x72%");
+
+$x->[0]->Write("png:".$filename.".png");
+
+
